e8d4d6fedb4990d327a8f70739f7392c9e942843
Mathieu Virbel <mat@kivy.org>
2017-10-01 15:58:29 +0200

    more changes, faster ui, more controls

--- looper.py
+++ looper.py
@@ -1,5 +1,8 @@
 #!/usr/bin/env python
 """
+Midi Looper
+===========
+
 UI:
     Display made for https://learn.adafruit.com/adafruit-pioled-128x32-mini-oled-for-raspberry-pi/
     Font are:
@@ -7,15 +10,18 @@ UI:
         https://fonts2u.com/fairfax-bold.font
 """
 
+import io
 import os
+import json
 import rtmidi
 import threading
+import traceback
 from time import time
-from functools import partial
 from pynput.keyboard import Listener, Key, KeyCode
 from rtmidi.midiconstants import (ALL_SOUND_OFF, CONTROL_CHANGE,
                                   RESET_ALL_CONTROLLERS, NOTE_ON,
-                                  NOTE_OFF)
+                                  NOTE_OFF, SONG_START,
+                                  SONG_CONTINUE, SONG_STOP)
 from PIL import Image, ImageDraw, ImageFont
 
 try:
@@ -27,47 +33,64 @@ except:
 
 global looper
 Char = KeyCode.from_char
-midi_in_callback = None
 SIZE = (128, 32)
-DPI = 10
+DPI = 5
 size = [x * DPI for x in SIZE]
 image = None
 
 
 class Track(object):
-    def __init__(self, index):
+    def __init__(self, index, midiout, looper):
         self.index = index
         self.notes = []
-        self.record = False
-        self.mute = False
+        self.notes_next = []
+        self.recording = False
+        self.muted = False
         self.on = []
+        self.midiout = midiout
+        self.looper = looper
 
-    def midi_in_callback(self, clock, m, data):
+    def quantize(self, clock, m):
+        diff = (clock % m)
+        clock -= diff
+        if diff > m / 2:
+            clock += m
+        return clock
+
+    def midiin_callback(self, clock, m, data):
         print("track {}: {} {}".format(self.index, clock, m, data))
-        if not self.record:
+        if not self.recording:
             return
         status = m[0] & 0xF0
         if status in (NOTE_ON, NOTE_OFF):
-            self.notes.append((clock, m, data))
+            if looper.quantize > 0:
+                clock = self.quantize(clock, 60 / looper.bpm / looper.quantize)
+            self.notes_next.append((clock, m, data))
+
+    def reset(self):
+        self.notes = []
+        self.notes_next = []
 
     def start_recording(self):
-        self.record = True
-        self.notes = []
+        self.recording = True
         self.off()
 
     def stop_recording(self):
-        self.record = False
+        self.recording = False
+        self.merge()
 
     def toggle_mute(self):
-        self.mute = not self.mute
-        if self.mute:
+        self.muted = not self.muted
+        if self.muted:
             self.off()
 
     def get_notes_between(self, start, end):
-        if self.record or self.mute:
+        if self.muted:
             return
+        if start == 0:
+            start = -1
         for clock, message, data in self.notes:
-            if clock >= start and clock < end:
+            if clock >= start and clock <= end:
                 yield message
 
     def send_notes_between(self, start, end):
@@ -86,33 +109,38 @@ class Track(object):
             self.midiout.send_message([NOTE_OFF | chn, note, 0])
         self.on = []
 
+    def merge(self):
+        self.notes += self.notes_next
+        self.notes_next = []
+
 
 class Player(threading.Thread):
-    def __init__(self):
+    def __init__(self, looper, midiout):
         super(Player, self).__init__()
-        self.is_playing = threading.Event()
-        self.is_playing.clear()
+        self._is_playing = threading.Event()
+        self._is_playing.clear()
         self.daemon = True
         self.quit = False
-        self.looper = None
+        self.looper = looper
         self.restart = False
         self.time_start = 0
+        self.midiout = midiout
         self.start()
-        self.midiout = rtmidi.MidiOut()
-        self.midiout.open_port(1)
+
+    @property
+    def playing(self):
+        return self._is_playing.is_set()
 
     def run(self):
         prev_deltatime = 0
         deltatime = 0
         play_notes = self.play_notes
+        looper = self.looper
 
         while not self.quit:
-            looper = self.looper
-            if not looper:
-                continue
 
-            if not self.is_playing.is_set():
-                self.is_playing.wait()
+            if not self.playing:
+                self._is_playing.wait()
                 continue
 
             if self.restart:
@@ -121,40 +149,47 @@ class Player(threading.Thread):
 
             length = looper.length
             prev_deltatime = deltatime
-            deltatime = (time() - self.time_start) % length
+            now = (time() - self.time_start)
+            deltatime = now % length
 
             if deltatime > prev_deltatime:
                 play_notes(looper, prev_deltatime, deltatime)
             else:
                 play_notes(looper, prev_deltatime, length)
+                self.merge_track_notes()
                 play_notes(looper, 0, deltatime)
 
     def play_notes(self, looper, start, stop):
         for track in list(looper.tracks.values()):
-            track.midiout = self.midiout
             track.send_notes_between(start, stop)
 
+    def merge_track_notes(self):
+        for track in list(looper.tracks.values()):
+            track.merge()
+
     def toggle_play(self):
-        if self.is_playing.is_set():
+        if self.playing:
             return self.stop()
         self.play()
 
     def play(self):
-        if self.is_playing.is_set():
+        if self.playing:
             return
         self.time_start = time()
         self.restart = True
-        self.is_playing.set()
+        self._is_playing.set()
+        self.midiout.send_message([SONG_START])
 
     def stop(self):
-        if not self.is_playing.is_set():
+        if not self.playing:
             return
-        self.is_playing.clear()
+        self._is_playing.clear()
+        self.midiout.send_message([SONG_STOP])
         self.notes_off()
 
     def panic(self):
-        send = self.midiout.send_message
-        self.notes_off()
+        send = self.looper.midiout.send_message
+        self.stop()
         for channel in range(16):
             send([CONTROL_CHANGE, ALL_SOUND_OFF, 0])
             send([CONTROL_CHANGE, RESET_ALL_CONTROLLERS, 0])
@@ -190,13 +225,29 @@ class Looper(object):
         Char("j"): ("mute", [7]),
         Char("k"): ("mute", [8]),
 
+        Char("+"): ("increment_measure", [1]),
+        Char("-"): ("increment_measure", [-1]),
+        Key.page_up: ("increment_tempo", [10]),
+        Key.page_down: ("increment_tempo", [-10]),
+        Key.home: ("increment_tempo", [1]),
+        Key.end: ("increment_tempo", [-1]),
+        Key.insert: "decrease_quantize",
+        Key.delete: "increase_quantize",
+
+        Key.f12: "save_settings",
+        Key.f11: "load_settings",
+
         Key.space: "toggle_play",
         Key.esc: "panic",
-        Key.caps_lock: "toggle_record_on_first_note"
+        Key.caps_lock: "toggle_record_on_first_note",
+        Char("/"): "midi_prev_port",
+        Char("*"): "midi_next_port"
     }
 
     shift_commands = {}
     ctrl_commands = {}
+
+    QUANTIZE = (1, 2, 3, 4, 8)
 
     def __init__(self):
         self.tracks = {}
@@ -205,11 +256,88 @@ class Looper(object):
         self._key_pressed = []
         self._record_to_track = None
         self.record_on_first_note = True
-        self.player = Player()
-        self.player.looper = self
         self.bpm = 120
         self.beat_per_measures = 4
         self.measures = 4
+        self.port = 0
+        self.quantize = 16
+        self.midiin = rtmidi.MidiIn()
+        self.midiout = rtmidi.MidiOut()
+        self.midiin_name = self.midiout_name = ""
+        self.player = Player(self, self.midiout)
+        self.recalculate_length()
+        self.open_midi_port()
+        self.load_settings()
+
+    def increase_quantize(self):
+        index = self.QUANTIZE.index(self.quantize)
+        index = min(len(self.QUANTIZE) - 1, (index + 1))
+        self.quantize = self.QUANTIZE[index]
+
+    def decrease_quantize(self):
+        index = self.QUANTIZE.index(self.quantize)
+        index = max(0, (index - 1))
+        self.quantize = self.QUANTIZE[index]
+
+    @property
+    def settings(self):
+        return {
+            "__version__": 1,
+            "bpm": self.bpm,
+            "measures": self.measures,
+            "beat_per_measures": self.beat_per_measures,
+            "port": self.port,
+            "record_on_first_note": self.record_on_first_note,
+            "quantize": self.quantize,
+            "tracks": self.dump_tracks()
+        }
+
+    def dump_tracks(self):
+        tracks = []
+        for track in self.tracks.values():
+            tracks.append({
+                "index": track.index,
+                "notes": track.notes,
+                "muted": track.muted
+            })
+        return tracks
+
+    def save_settings(self):
+        with io.open("settings.json", "w", encoding="utf-8") as fd:
+            json.dump(self.settings, fd)
+        print("Settings saved")
+
+    def load_settings(self):
+        try:
+            with open("settings.json") as fd:
+                settings = json.load(fd)
+            if settings["__version__"] <= 1:
+                self.bpm = settings["bpm"]
+                self.beat_per_measures = settings["beat_per_measures"]
+                self.measures = settings["measures"]
+                self.record_on_first_note = settings["record_on_first_note"]
+                self.port = settings["port"]
+                self.quantize = settings["quantize"]
+                self.recalculate_length()
+                self.open_midi_port()
+
+                self.tracks = {}
+                for info in settings["tracks"]:
+                    track = self.get_track(info["index"])
+                    track.notes = info["notes"]
+                    track.muted = info["muted"]
+
+            print("Settings loaded")
+        except:
+            print("Error while loading settings")
+            traceback.print_exc()
+
+    def increment_tempo(self, amount):
+        self.bpm = min(240, max(60, (self.bpm + amount)))
+        self.recalculate_length()
+
+    def increment_measure(self, amount):
+        self.measures = min(24, max(1, self.measures + amount))
         self.recalculate_length()
 
     def recalculate_length(self):
@@ -217,21 +345,43 @@ class Looper(object):
         self.measure_length = self.beat_length * self.beat_per_measures
         self.length = self.measure_length * self.measures
 
+    def open_midi_port(self):
+        if self.midiin.is_port_open():
+            self.midiin.close_port()
+        if self.midiout.is_port_open():
+            self.midiout.close_port()
+        self.midiin.open_port(self.port)
+        self.midiout.open_port(self.port)
+        self.midiin.set_callback(self.midiin_callback)
+        self.midiin_name = self.midiin.get_port_name(self.port)
+        self.midiout_name = self.midiout.get_port_name(self.port)
+        self.player.midiout = self.midiout
+        for track in self.tracks.values():
+            track.midiout = self.midiout
+        print("Connected: in={} out={}".format(self.midiin_name, self.midiout_name))
+
+    def midi_next_port(self):
+        self.port = (self.port + 1) % len(self.midiin.get_ports())
+        self.open_midi_port()
+
+    def midi_prev_port(self):
+        self.port = (self.port - 1) % len(self.midiin.get_ports())
+        self.open_midi_port()
+
     @property
     def measure(self):
-        if not self.player.is_playing.is_set():
+        if not self.player.playing:
             return 1
         return 1 + int(self.player.deltatime / self.measure_length) % self.measures
 
     @property
     def beat(self):
-        if not self.player.is_playing.is_set():
-            return 1
+        if not self.player.playing:
+            return 0
         return 1 + int((self.player.deltatime / self.beat_length) % self.beat_per_measures)
 
     def reset(self):
         self.stop()
-        self.length = None
         self.tracks = {}
 
     def panic(self):
@@ -252,19 +402,37 @@ class Looper(object):
         self.record_on_first_note = not self.record_on_first_note
 
     def record(self, index):
+        prev_index = 0
+        if self._record_to_track:
+            self._record_to_track.stop_recording()
+            prev_index = self._record_to_track.index
+        self._record_to_track = track = self.get_track(index)
+        if track.index == prev_index:
+            self._record_to_track = None
+            return
+        if self.ctrl:
+            track.reset()
+            return
         if not self.record_on_first_note:
             self.play()
-        self._record_to_track = track = self.get_track(index)
         track.start_recording()
 
     def mute(self, index):
         self.get_track(index).toggle_mute()
 
-    def record_after(self, index):
+    def _record_after(self, index):
         self._record_to_track.stop_recording()
         self._record_to_track = None
 
-    def midi_in_callback(self, wallclock, message, data):
+    def midiin_callback(self, blob, data):
+        message, deltatime = blob
+
+        # control part
+        if message[0] == SONG_START:
+            self.play()
+        elif message[0] == SONG_STOP:
+            self.stop()
+
         if not self._record_to_track:
             return
 
@@ -272,21 +440,21 @@ class Looper(object):
         if not is_note:
             return
         if self.record_on_first_note:
-            if not self.player.is_playing.is_set():
+            if not self.player.playing:
                 self.player.play()
         deltatime = self.player.deltatime % self.length
-        self._record_to_track.midi_in_callback(deltatime, message, data)
+        self._record_to_track.midiin_callback(deltatime, message, data)
 
     def get_track(self, index):
         if index not in self.tracks:
-            self.tracks[index] = Track(index)
+            self.tracks[index] = Track(index, self.midiout, self)
         return self.tracks[index]
 
     def dispatch_command(self, key, after=False):
         cmd, func, args = self.get_command(key, after)
         if not cmd:
             return
-        print("Execute: {}".format(cmd))
+        print("Execute: {} {}".format(cmd, args))
         func(*args)
 
     def get_command(self, key, after=False):
@@ -312,6 +480,7 @@ class Looper(object):
         return cmd, func, args
 
     def on_key_pressed(self, key):
+        print("key", key)
         if key == Key.shift:
             self.shift = True
         elif key == Key.ctrl:
@@ -340,8 +509,7 @@ class MidiInputHandler(object):
     def __call__(self, event, data=None):
         message, deltatime = event
         self._wallclock += deltatime
-        if looper:
-            looper.midi_in_callback(self._wallclock, message, data)
+        looper.midiin_callback(self._wallclock, message, data)
         print("[%s] @%0.6f %r" % (self.port, self._wallclock, message))
 
 
@@ -353,6 +521,7 @@ class UI(threading.Thread):
         self.draw = ImageDraw.Draw(self.image)
         self.font = ImageFont.truetype("fonts/Fairfax.ttf", size=10 * DPI)
         self.daemon = True
+        self.cache = None
         self.start()
 
     def run(self):
@@ -363,50 +532,73 @@ class UI(threading.Thread):
             self.image1 = ImageTk.PhotoImage(self.image)
             self.panel = tk.Label(self.root, image=self.image1)
             self.panel.pack()
-            self.root.after(50, self.render_and_display)
+            self.root.after(1, self.render_and_display)
             self.root.mainloop()
+            os._exit(0)
 
     def render_and_display(self):
-        self.render()
-        self.display()
-        self.root.after(50, self.render_and_display)
+        if self.render():
+            self.display()
+        if tk:
+            self.root.after(1, self.render_and_display)
 
     def render(self):
-        # ▶◼⚪⚫ (~25 chars width)
+        # ▶■◼⚪⚫ (~25 chars width)
         width, height = size
         d = self.draw
         font = self.font
         player = looper.player
 
         def text(x, y, message):
-            d.text((x * DPI, y * DPI), message, font=font, fill=255)
+            mx = 2
+            d.text(((mx + x) * DPI, y * DPI), message, font=font, fill=255)
 
-        d.rectangle((0, 0, width, height), outline=0, fill=0)
-
-        state = u"‣" if player.is_playing.is_set() else u"▪"
-        top = "{state} {bpm} {measure}-{measures} {beat}{beatleft}".format(
+        state = u"‣" if player.playing else u"■"
+        quantize = "{}".format(looper.quantize).ljust(2)
+        top = "{state} {bpm} {measure}-{measures} {quantize} {beat}{beatleft} {port}".format(
             state=state,
             bpm=looper.bpm,
             measure=looper.measure,
             measures=looper.measures,
             beat="⚫" * looper.beat,
-            beatleft="⚪" * (looper.beat_per_measures - looper.beat)
+            beatleft="⚪" * (looper.beat_per_measures - looper.beat),
+            port=looper.midiin_name,
+            quantize=quantize
         )
+
+        def render_status(index):
+            status = ""
+            if index in looper.tracks:
+                track = looper.get_track(index)
+                status += "⚫" if track.notes else "⚪"
+                status += "R" if track.recording else " "
+                status += "M" if track.muted else " "
+                status += str(len(track.notes) + len(track.notes_next))
+            else:
+                status = "⚪"
+            return status.ljust(8)
+
+        line1 = "".join([render_status(i) for i in (1, 4, 7)])
+        line2 = "".join([render_status(i) for i in (2, 5, 8)])
+        line3 = "".join([render_status(i) for i in (3, 6, 9)])
+
+        cache = [top, line1, line2, line3]
+        if self.cache == cache:
+            return False
+        self.cache = cache
+
+        d.rectangle((0, 0, width, height), outline=0, fill=0)
+        text(0, 0, top)
+        text(0, 8, line1)
+        text(0, 16, line2)
+        text(0, 24, line3)
+        return True
 
     def display(self):
         if tk:
-            self.image1 = ImageTk.PhotoImage(self.image)
+            self.image1 = ImageTk.BitmapImage(self.image)
             self.panel.configure(image=self.image1)
-
-
-def on_keyboard_pressed(key):
-    if looper:
-        looper.on_key_pressed(key)
-
-
-def on_keyboard_released(key):
-    if looper:
-        looper.on_key_released(key)
+            self.root.update()
 
 
 def hide_keyboard():
@@ -421,15 +613,12 @@ try:
     hide_keyboard()
 
     looper = Looper()
-    midi_in = rtmidi.MidiIn()
-    port = 1
-    port_name = midi_in.get_ports()[port]
-    midi_in.set_callback(MidiInputHandler(port_name))
-    midi_in.open_port(port)
 
     ui = UI()
 
-    with Listener(on_press=on_keyboard_pressed, on_release=on_keyboard_released) as listener:
+    with Listener(
+            on_press=looper.on_key_pressed,
+            on_release=looper.on_key_released) as listener:
         listener.join()
 
 finally:
